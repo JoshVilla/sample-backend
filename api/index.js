067fee651539cb79af0b7b2ab4a12da49e4f51c3
JoshVilla <villaearljoshua@gmail.com>
2025-09-24 10:53:14 +0800

    add api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const AdminSchema = new mongoose.Schema({ name: String });
+const Admin = mongoose.models.User || mongoose.model("User", AdminSchema);
 // Test route
 app.get("/api/health", (req, res) => {
   res.json({ message: "✅ Backend is running on Vercel!" });
 });
 
 // ✅ export app instead of app.listen()
+
+app.post("/api/admin", async (req, res) => {
+  const admin = new Admin({ data: req.body.name });
+  await admin.save();
+  res.json(admin);
+});
 export default app;
--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(cors());
 app.use(express.json());
 
 // Test route
-app.get("/api/health", (req, res) => {
-  res.json({ message: "✅ Backend is running on Render!" });
-});
+// app.get("/api/health", (req, res) => {
+//   res.json({ message: "✅ Backend is running on Render!" });
+// });
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
