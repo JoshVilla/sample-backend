954774f362b1b91ef68e7051c94af21b74cccf44
JoshVilla <villaearljoshua@gmail.com>
2025-09-24 14:56:41 +0800

    fix

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import adminRoutes from "./adminRoutes.js";
-import addAdminRoutes from "./addAdminRoutes.js";
+// import addAdminRoutes from "./addAdminRoutes.js";
 const router = express.Router();
 
 // Mount individual route modules
 router.use("/admins", adminRoutes);
-router.use("/admin/add", addAdminRoutes);
+// router.use("/admin/add", addAdminRoutes);
 
 export default router;
